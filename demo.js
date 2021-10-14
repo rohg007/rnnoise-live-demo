268bae8dec834dc48135dbd60385f5c5c5a618a9
gupta <17dcs012@lnmiit.ac.in>
2021-10-14 14:44:24 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,4 @@
+function init_demo(){
+    live_noise(0, document.getElementById("default_live_noise"));
+    live_noise_suppression(0, document.getElementById("default_live_noise_suppression"));
+}
